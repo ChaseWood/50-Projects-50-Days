283d6b1ccf32b8d911500e4c38921b5ce39b66fd
Chase Wood <chase_wood07@yahoo.com>
2021-01-13 19:47:21 -0600

    finished progressSteps.js

--- progressSteps/script.js
+++ progressSteps/script.js
@@ -18,7 +18,6 @@ prev.addEventListener('click', () => {
 	if (currentActive < 1) {
 		currentActive = 1;
 	}
-
 	update();
 });
 
@@ -30,4 +29,18 @@ function update() {
 			circle.classList.remove('active');
 		}
 	});
+
+	const actives = document.querySelectorAll('.active');
+
+	progress.style.width =
+		((actives.length - 1) / (circles.length - 1)) * 100 + '%';
+
+	if (currentActive === 1) {
+		prev.disabled = true;
+	} else if (currentActive === circles.length) {
+		next.disabled = true;
+	} else {
+		prev.disabled = false;
+		next.disabled = false;
+	}
 }
